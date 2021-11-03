6fea9505406ccf94616d442b493928cfb01b2c6f
Arnab Chatterjee <arnabc1701@gmail.com>
2021-11-03 19:57:39 +0530

    sfgzs

--- api-server.js
+++ api-server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = process.env.PORT || 3002
 
 let supertokens = require("supertokens-node");
 let Session = require("supertokens-node/recipe/session");
